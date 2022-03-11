85e08ca59a4c1ab82170975bc4b6a23341ff5abd
kaioenzo <kaioenzobr@gmail.com>
2022-03-11 15:37:15 -0300

    Fix bug rel=noreferrer

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/kaioenzo/dsmovie" target="_blank"> 
+          <a href="https://github.com/kaioenzo/dsmovie" target="_blank" rel="noreferrer"> 
             <div className='dsmovie-contact-container'>
               <GitHubIcon />
               <p className='dsmovie-contact-link'>/kaioenzo</p>

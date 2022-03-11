7fb861311a5831c8a44520bfecbccc4ff1486abd
kaioenzo <kaioenzobr@gmail.com>
2022-03-11 15:33:51 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,36 +1,76 @@
 import './style.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { BASE_URL } from 'ultils/requests';
+import { validateEmail } from 'ultils/validate';
 
 type Props = {
     movieId: string;
 }
 
-function FormCard({movieId} : Props) {
- 
+function FormCard({ movieId }: Props) {
+
+    const navigate = useNavigate();
+
     const [movie, setMovie] = useState<Movie>();
 
-    useEffect(() =>{
+    useEffect(() => {
         axios.get(`${BASE_URL}/movies/${movieId}`)
-        .then(response =>{
-            setMovie(response.data);
-        });
-    },[movieId]); 
+            .then(response => {
+                setMovie(response.data);
+            });
+    }, [movieId]);
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+        if (!validateEmail(email)){
+            return;
+        }
+
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieId,
+                score: score
+            }
+        }
+
+        axios(config).then(response => {
+            navigate("/")
+        });
+
+       // eslint-disable-next-line no-lone-blocks
+       {/* const text = (event.target as any).text.value;*/} 
+
+
+    }
 
     return (
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie?.title}</h3>
-                <form className="dsmovie-form">
+                <form className="dsmovie-form" onSubmit={handleSubmit}>
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
                     </div>
+
+                    {/* 
+                <div className="form-group dsmovie-form-group">
+                        <label htmlFor="text">Escreva seu comentário</label>
+                        <input type="text" className="form-control" id="text" />
+                    </div> 
+                    */}
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="score">Informe sua avaliação</label>
                         <select className="form-control" id="score">
